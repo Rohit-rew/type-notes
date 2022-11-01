899ed8515fd7ee808d881624b531019977d1fc96
rohit <rohit.rew@gmail.com>
2022-11-01 23:47:43 +0530

    header created

---
+++ src/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+
+    <nav className='flex justify-center shadow '>
+        <p className='text-blue-600 text-2xl '>Lets Note</p>
+    </nav>
+  )
+}
